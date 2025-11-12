19facc25e8f76f772e2c90298de1979df9638b72
Gag <256sha9gag@gmail.com>
2025-11-12 17:21:09 +0300

    fix: umd-bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+// TODO: added radix ui external dependencies
+
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-// TODO: added radix ui external dependencies
 export default defineConfig({
     plugins: [
         react(),
@@ -20,8 +21,8 @@ export default defineConfig({
         lib: {
             entry: path.resolve('', 'src/lib/index.ts'),
             name: 'QWEP-KIT',
-            formats: ['es', 'umd'],
-            fileName: (format) => `qwep-kit.${format}.js`,
+            formats: ['umd'],
+            fileName: () => `qwep-kit.umd.js`,
         },
         rollupOptions: {
             external: ['react', 'react-dom', 'styled-components'],
